c80a9fa3b33e6817fea5b76983fba36547570406
Manuel Martinez <manuel.f.m.soto@gmail.com>
2018-02-09 10:48:22 -0700

    Changes to pwem2d.m Needed: Determine if K is needed as function input.

--- pwem2d.m
+++ pwem2d.m
@@ -1,5 +1,5 @@
-function K0 = pwem2d(DEV,SYM,RES,PQR,MODE)
-  % K0 = pwem2d(DEV,GRID,K,PQR,MODE) Plane Wave Expansion Method
+function K0 = pwem2d(DEV,SYM,RES,PQ,MODE)
+  % K0 = pwem2d(DEV,GRID,RES,PQ,MODE) Plane Wave Expansion Method
   %
   % INPUT ARGUMENTS
   % ============================================================================
@@ -9,21 +9,29 @@ function K0 = pwem2d(DEV,SYM,RES,PQR,MODE)
   %   .T1       Reciprocal lattice vector
   %   .T2       Reciprocal lattice vector
   %
-  % SYM         Key points of symmetry (for IBZ). Can be used with N points of 
+  % SYM         Key points of symmetry (for IBZ). Can be used with N points of
   %             symmetry.
   %   .NP       Number of points to be used in band diagram calculation
   %   .POINTS   Array containing all symmetry points
-  %   
+  %
   % RES         Grid resolution
   %   RES(1)    x-axis resolution
   %   RES(2)    y-axis resolution
   %
-  % PQR         Number of harmonics along P,Q,and R
-  %   .P        
+  % PQ          Number of harmonics along P,Q,and R
+  %   .P
   %   .Q
-  %   .R
   %
   % MODE        Mode structure containing
   %   .EM       Electromagnetic mode 'E' or 'H'
   %   .BC       1 = Band Diagram, 2 = Complete Band Diagram
-  
+
+  % DETERMINE GRID SIZE
+  [Nx,Ny] = size(ER);
+
+  % EXTRACT GRID PARAMETERS
+  dx = RES(1);
+  dy = RES(2);
+
+  % DETERMINE TOTAL NUMBER OF HARMONICS
+  M = PQ.P * PQ.Q;
