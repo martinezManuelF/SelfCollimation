3124cf9931ae3138c3b4fb7e550e95f518abe063
TasartirAmras <manuel.f.m.soto@gmail.com>
2018-02-12 23:15:08 -0700

    Major changes to simulation code and pwem2d

--- pwem2d.m
+++ pwem2d.m
@@ -1,5 +1,5 @@
-function WN = pwem2d(DEV,SYM,BETA,RES,PQ,MODE)
-  % K0 = pwem2d(DEV,GRID,RES,PQ,MODE) Plane Wave Expansion Method
+function WN = pwem2d(DEV,SYM,BETA,PQ,MODE)
+  % WN = pwem2d(DEV,GRID,RES,PQ,MODE) Plane Wave Expansion Method
   %
   % INPUT ARGUMENTS
   % ============================================================================
@@ -17,28 +17,19 @@ function WN = pwem2d(DEV,SYM,BETA,RES,PQ,MODE)
   %
   % BETA        2-by-? Array of BLOCH wave vectors
   %
-  % RES         Grid resolution
-  %   RES(1)    x-axis resolution
-  %   RES(2)    y-axis resolution
-  %
   % PQ          Number of harmonics along P,Q,and R
   %   .P
   %   .Q
   %
   % MODE        Mode structure containing
   %   .EM       Electromagnetic mode 'E' or 'H'
-  %   .BC       1 = Band Diagram, 2 = Complete Band Diagram
   %
   % OUTPUT ARGUMENTS
   % ============================================================================
   % WN          Normalized frequency
 
   % DETERMINE GRID SIZE
-  [Nx,Ny] = size(ER);
-
-  % EXTRACT GRID PARAMETERS
-  dx = RES(1);
-  dy = RES(2);
+  [Nx,Ny] = size(DEV.ER);
 
   % DETERMINE TOTAL NUMBER OF HARMONICS
   M = PQ.P * PQ.Q;
@@ -58,50 +49,49 @@ function WN = pwem2d(DEV,SYM,BETA,RES,PQ,MODE)
   q = [ -floor(PQ.Q/2) : +floor(PQ.Q/2) ];
 
   % INITIALIZE NORMALIZED FREQUENCY ARRAYS
-  if (MODE.BC == 1)
-    if (MODE.EM == 'E')
-      WNE = zeros(M,NBX);
-    elseif (MODE.EM == 'H')
-      WNH = zeros(M,NBX);
-    end
-  elseif (MODE.BC == 2)
-  return;
+  if (MODE.EM == 'E')
+    WNE = zeros(M,NBX);
+  elseif (MODE.EM == 'H')
+    WNH = zeros(M,NBX);
   end
-
+  
   % SOLVE PROBLEM
-  if (MODE.BC == 1)
-    for nbeta = 1 : NBX
-      KX = bx(nbeta) - 2*pi*p/DEV.LATTICE;
-      KY = by(nbeta) - 2*pi*q/DEV.LATTICE;
-      [KY,KX] = meshgrid(KY,KX);
-      KX = diag(sparse(KX(:)));
-      KY = diag(sparse(KY(:)));
-        if (MODE.EM == 'E')
-          % Build eigen-value problem
-          A = KX/URC*KX + KY/URC*KY;
-          B = ERC;
+  for nbeta = 1 : NBX
+    KX = bx(nbeta) - 2*pi*p/DEV.LATTICE;
+    KY = by(nbeta) - 2*pi*q/DEV.LATTICE;
+    [KY,KX] = meshgrid(KY,KX);
+    KX = diag(sparse(KX(:)));
+    KY = diag(sparse(KY(:)));
+    if (MODE.EM == 'E')
+      % Build eigen-value problem
+      A = KX/URC*KX + KY/URC*KY;
+      B = ERC;
 
-          % Solve eigen-value problem
-          [K0] = eig(A,B);
+      % Solve eigen-value problem
+      [K0] = eig(A,B);
 
-          % Record normalized frequencies
-          K0 = sort(K0);
-          K0 = (DEV.LATTICE/(2*pi))*real(sqrt(K0));
-          WNE(:,nbeta) = K0;
-        elseif (MODE.EM == 'H')
-          % Build eigen-value problem
-          A = KX/ERC*KX + KY/ERC*KY;
-          B = URC;
+      % Record normalized frequencies
+      K0 = sort(K0);
+      K0 = (DEV.LATTICE/(2*pi))*real(sqrt(K0));
+      WNE(:,nbeta) = K0;
+    elseif (MODE.EM == 'H')
+      % Build eigen-value problem
+      A = KX/ERC*KX + KY/ERC*KY;
+      B = URC;
 
-          % Solve eigen-value problem
-          [K0] = eig(A,B);
+      % Solve eigen-value problem
+      [K0] = eig(A,B);
 
-          % Record normalized frequencies
-          K0 = sort(K0);
-          K0 = (DEV.LATTICE/(2*pi))*real(sqrt(K0));
-          WNH(:,nbeta) = K0;
-        end
-      end
-    elseif (MODE.BC == 2)
-    return;
+      % Record normalized frequencies
+      K0 = sort(K0);
+      K0 = (DEV.LATTICE/(2*pi))*real(sqrt(K0));
+      WNH(:,nbeta) = K0;
     end
+  end
+  
+  if (MODE.EM == 'E')
+      WN = WNE;
+  elseif (MODE.EM == 'H')
+      WN = WNH;
+  end
+end
